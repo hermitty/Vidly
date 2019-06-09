d3fd73c4d6944d638badb4b72ac4fdfb002a8b0f
hermitty <47337808+hermitty@users.noreply.github.com>
2019-06-09 12:31:21 +0200

    Init project

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Movie movie = GetMovies().SingleOrDefault(c => c.Id == id);
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id = 1, Title = "Harry Potter 1"},
+                new Movie{Id = 2, Title = "Harry Potter 2"}
+            };
+        }
+    }
+}
